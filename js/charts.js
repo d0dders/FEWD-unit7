c77bfad8b56b28d95373bc04af3ef0c08dcdcdde
d0dders <dodders@outlook.com>
2020-01-08 20:30:23 +0000

    Fixed issue with responsive grid on shrink

--- js/charts.js
+++ js/charts.js
@@ -39,77 +39,78 @@ var myChart = new Chart(ctx, {
     }
 });
 
-// /*---------------------------------------*/
-// /*             DAILY TRAFFIC             */
-// /*---------------------------------------*/
-// var ctx = document.getElementById('daily-traffic');
-// var myChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-//         datasets: [{
-//             label: '# of Visits',
-//             data: [50, 75, 150, 100, 200, 180, 75],
-//             backgroundColor: [
-//                 '#535993',
-//                 '#535993',
-//                 '#535993',         
-//                 '#535993',
-//                 '#535993',
-//                 '#535993',
-//                 '#535993'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         legend: {
-//             display: false,
-//          },
-//         scales: {
-//             yAxes: [{
+/*---------------------------------------*/
+/*             DAILY TRAFFIC             */
+/*---------------------------------------*/
+var ctx = document.getElementById('daily-traffic');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+        datasets: [{
+            label: '# of Visits',
+            data: [50, 75, 150, 100, 200, 180, 75],
+            backgroundColor: [
+                '#535993',
+                '#535993',
+                '#535993',         
+                '#535993',
+                '#535993',
+                '#535993',
+                '#535993'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            display: false,
+         },
+        scales: {
+            yAxes: [{
                 
-//                 ticks: {
-//                     beginAtZero: true,
-//                     max: 250
-//                 },
-//                 gridLines: {
-//                     offsetGridLines: true
-//                 }
-//             }]
-//         }
-//     }
-// });
+                ticks: {
+                    beginAtZero: true,
+                    max: 250
+                },
+                gridLines: {
+                    offsetGridLines: true
+                }
+            }]
+        }
+    }
+});
 
-// /*---------------------------------------*/
-// /*             MOBILE USERS           */
-// /*---------------------------------------*/
-// var ctx = document.getElementById('mobile-users');
-// var myDoughnutChart = new Chart(ctx, {
-//     type: 'doughnut',
-//     data: {
-//         labels: ["Phone", "Tablet", "Desktop"],
-//         datasets: [
-//         {
-//             label: "User platforms",
-//             data: [15, 15, 70],
-//             backgroundColor: [
-//             "#DEB887",
-//             "#A9A9A9",
-//             "#DC143C"
-//             ],
-//         }
-//         ]
-//     },
-//     options: {
-//         legend: {
-//           display: true,
-//           position: "right",
-//           labels: {
-//             fontColor: "#333",
-//             fontSize: 16
-//           }
-//         }
-//       }
+/*---------------------------------------*/
+/*             MOBILE USERS           */
+/*---------------------------------------*/
+var ctx = document.getElementById('mobile-users');
+var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ["Phone", "Tablet", "Desktop"],
+        datasets: [
+        {
+            label: "User platforms",
+            data: [15, 15, 70],
+            backgroundColor: [
+            "#DEB887",
+            "#A9A9A9",
+            "#DC143C"
+            ],
+        }
+        ]
+    },
+    options: {
+        legend: {
+          display: true,
+          position: "right",
+          labels: {
+            fontColor: "#333",
+            fontSize: 16
+          }
+        }
+      }
     
-// });
+});
