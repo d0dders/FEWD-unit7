fb00c35b120c8e0ae4c7bbb2f1171fd6e6e412f5
d0dders <dodders@outlook.com>
2020-01-14 21:17:52 +0000

    Add alert notifiction and made header fit better on mobile

--- js/app.js
+++ js/app.js
@@ -13,3 +13,23 @@ nav.addEventListener('click', (e) => {
         target.className = "active-nav";
     };
 });
+
+/*----------------------------------------------*/
+/*                  ALERT                       */
+/*----------------------------------------------*/
+const alertBanner = document.getElementById("alert");
+
+alertBanner.innerHTML = 
+    `
+    <div class="alert-banner">
+        <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+        <p class="alert-banner-close">X</p>
+    </div>
+    `;
+
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+    alertBanner.style.display = "none"
+    }
+});
