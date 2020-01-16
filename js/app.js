6b84afc59d9fbbadad10a8ed7ea7ccaa4f5acc96
d0dders <dodders@outlook.com>
2020-01-16 21:14:39 +0000

    Add some JS behind messaging form

--- js/app.js
+++ js/app.js
@@ -33,3 +33,23 @@ alertBanner.addEventListener('click', e => {
     alertBanner.style.display = "none"
     }
 });
+
+/*-----------------------------------------------*/
+/*                MESSAGING                      */
+/*-----------------------------------------------*/
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+send.addEventListener('click', (event) => {
+    if (user.value === "" && message.value === "") {
+        alert("Please fill out user and message fields before sending");
+    } else if (user.value === "") {
+        alert("Please fill out user field before sending");
+    } else if (message.value === "") {
+        alert("Please fill out message field before sending");
+    } else {
+        alert(`Message successfully sent to ${user.value}`);
+    }
+    event.preventDefault();
+});
